01741ea7265819f5d7e8c1b149a474419ea752ac
LeHPhuc <plehoang641@gmail.com>
2024-12-05 14:31:02 +0700

    updates

--- hotelmanagerapp/__init__.py
+++ hotelmanagerapp/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
-
+from urllib.parse import quote
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/hotelmanagementapp?charset=utf8mb4" % quote("060204")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app=app)
